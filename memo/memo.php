4fd45fdea9f26a9c838ee18e92d2e1a151759d98
Galaxym <simon.uldry@formation.orif.ch>
2015-10-02 20:55:28 +0200

    Ajout du memo + sa cible

--- memo/memo.php
+++ memo/memo.php
@@ -1,5 +1,14 @@
 		<?php include '../templates/header.php'; ?>
         <h2>Mémo</h2>
-        <p>Ajouter ici le formulaire ...</p>
-        <p></p>
+        <form method="POST" action="memoform.php">
+        	<table>
+        		<tr>
+        			<td colspan="2"><textarea name="memotext" style="width: 300px; height: 100px;"></textarea></td>
+        		</tr>
+        		<tr>
+        			<td><input type="submit"></td>
+        			<td><input type="reset" value="Effacer"></td>
+        		</tr>
+        	</table>
+        </form>
         <?php include '../templates/footer.php'; ?>
